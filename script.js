9580ff9612156179d4ac1ab643d978839e49b364
xjjo <hjoliva@gmail.com>
2016-10-11 14:10:48 -0300

    Agrego plugin para webchat

---
+++ script.js
@@ -0,0 +1,9 @@
+/*! CotorraChatbot v0.1 http://cotorrachatbot.com | (c) 2016 Ingeros */
+if (typeof jQuery != 'undefined') {
+
+    alert("jQuery library is loaded!. Cotorra will not work.");
+
+}else{
+
+    alert("jQuery library is not found! Cotorra will work.");
+}
