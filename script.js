0cd934c9ff114d83e7ef4a67759f9c5f0993bd76
xjjo <hjoliva@gmail.com>
2016-10-11 17:01:28 -0300

    Agrego plugin para webchat

--- script.js
+++ script.js
@@ -35,15 +35,17 @@ function initCotorra(data){
 	    alert("jQuery library is not found! Cotorra will not work.");
 	}
 	ctGlobalURL = "http://cotorrachatbot.com"
+	ctGlobalURL = "cotorra.github.io"
 	ctHeader = document.getElementsByTagName('head')[0],
 	ctBody = document.getElementsByTagName('body')[0],
 	appendCSS();
 
 	var ctdiv = document.createElement("div");
+	ctdiv.className = 'navbar-fixed-bottom col-md-3';
     ctdiv.innerHTML =
 		"<div class=\"box box-danger direct-chat direct-chat-danger\">" +
 		  "<div class=\"box-header with-border\">" +
-		    "<h3 class=\"box-title\">Direct Chat</h3>" +
+		    "<h3 class=\"box-title\">Chat</h3>" +
 		    "<div class=\"box-tools pull-right\">" +
 		      "<span data-toggle=\"tooltip\" title=\"3 New Messages\" class=\"badge bg-red\">3</span>" +
 		      "<button class=\"btn btn-box-tool\" data-widget=\"collapse\"><i class=\"fa fa-minus\"></i></button>" +
@@ -61,7 +63,7 @@ function initCotorra(data){
 		          "<span class=\"direct-chat-name pull-left\">Alexander Pierce</span>" +
 		          "<span class=\"direct-chat-timestamp pull-right\">23 Jan 2:00 pm</span>" +
 		        "</div><!-- /.direct-chat-info -->" +
-		        "<img class=\"direct-chat-img\" src=\"../dist/img/user1-128x128.jpg\" alt=\"message user image\"><!-- /.direct-chat-img -->" +
+		        "<img class=\"direct-chat-img\" src=\"http://bootdey.com/img/Content/user_2.jpg\" alt=\"message user image\"><!-- /.direct-chat-img -->" +
 		        "<div class=\"direct-chat-text\">" +
 		          "Is this template really for free? That's unbelievable!" +
 		        "</div><!-- /.direct-chat-text -->" +
@@ -73,40 +75,12 @@ function initCotorra(data){
 		          "<span class=\"direct-chat-name pull-right\">Sarah Bullock</span>" +
 		          "<span class=\"direct-chat-timestamp pull-left\">23 Jan 2:05 pm</span>" +
 		        "</div><!-- /.direct-chat-info -->" +
-		        "<img class=\"direct-chat-img\" src=\"../dist/img/user3-128x128.jpg\" alt=\"message user image\"><!--direct-chat-img -->" +
+		        "<img class=\"direct-chat-img\" src=\"http://bootdey.com/img/Content/user_2.jpg\" alt=\"message user image\"><!--direct-chat-img -->" +
 		        "<div class=\"direct-chat-text\">" +
 		          "You better believe it!" +
 		        "</div><!-- /.direct-chat-text -->" +
 		      "</div><!-- /.direct-chat-msg -->" +
-		    "</div><!--/.direct-chat-messages-->" +
-
-		    "<!-- Contacts are loaded here -->" +
-		    "<div class=\"direct-chat-contacts\">" +
-		      "<ul class=\"contacts-list\">" +
-		        "<li>" +
-		          "<a href=\"#\">" +
-		            "<img class=\"contacts-list-img\" src=\"../dist/img/user1-128x128.jpg\" alt=\"Contact Avatar\">" +
-		            "<div class=\"contacts-list-info\">" +
-		              "<span class=\"contacts-list-name\">" +
-		                "Count Dracula" +
-		                "<small class=\"contacts-list-date pull-right\">2/28/2015</small>" +
-		              "</span>" +
-		              "<span class=\"contacts-list-msg\">How have you been? I was...</span>" +
-		            "</div><!-- /.contacts-list-info -->" +
-		          "</a>" +
-		        "</li><!-- End Contact Item -->" +
-		      "</ul><!-- /.contatcts-list -->" +
-		    "</div><!-- /.direct-chat-pane -->" +
-		  "</div><!-- /.box-body -->" +
-		  "<div class=\"box-footer\">" +
-		    "<div class=\"input-group\">" +
-		      "<input type=\"text\" name=\"message\" placeholder=\"Type Message ...\" class=\"form-control\">" +
-		      "<span class=\"input-group-btn\">" +
-		        "<button type=\"button\" class=\"btn btn-danger btn-flat\">Send</button>" +
-		      "</span>" +
-		    "</div>" +
-		  "</div><!-- /.box-footer-->" +
-		"</div><!--/.direct-chat -->";
+		    "</div><!--/.direct-chat-messages-->";
 
 	ctBody.appendChild(ctdiv);	
 
