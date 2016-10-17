a90142651801fc27bb82bd54672dbce090b0af1d
asperduti <arielbmx@gmail.com>
2016-10-17 13:50:27 -0300

    Fix initial status webpluginchat

--- script.js
+++ script.js
@@ -116,27 +116,14 @@ function initCotorra(data){
 		      "<!-- Message. Default to the left -->" +
 		      "<div class=\"direct-chat-msg\">" +
 		        "<div class=\"direct-chat-info clearfix\">" +
-		          "<span class=\"direct-chat-name pull-left\">Alexander Pierce</span>" +
+		          "<span class=\"direct-chat-name pull-left\">Laura Tarot</span>" +
 		          "<span class=\"direct-chat-timestamp pull-right\">23 Jan 2:00 pm</span>" +
 		        "</div><!-- /.direct-chat-info -->" +
 		        "<img class=\"direct-chat-img\" src=\"http://bootdey.com/img/Content/user_2.jpg\" alt=\"message user image\"><!-- /.direct-chat-img -->" +
 		        "<div class=\"direct-chat-text\">" +
-		          "Is this template really for free? That's unbelievable!" +
+		          "Hola!" +
 		        "</div><!-- /.direct-chat-text -->" +
 		      "</div><!-- /.direct-chat-msg -->" +
-
-		      "<!-- Message to the right -->" +
-		      "<div class=\"direct-chat-msg right\">" +
-		        "<div class=\"direct-chat-info clearfix\">" +
-		          "<span class=\"direct-chat-name pull-right\">Sarah Bullock</span>" +
-		          "<span class=\"direct-chat-timestamp pull-left\">23 Jan 2:05 pm</span>" +
-		        "</div><!-- /.direct-chat-info -->" +
-		        "<img class=\"direct-chat-img\" src=\"http://bootdey.com/img/Content/user_2.jpg\" alt=\"message user image\"><!--direct-chat-img -->" +
-		        "<div class=\"direct-chat-text\">" +
-		          "You better believe it!" +
-		        "</div><!-- /.direct-chat-text -->" +
-		      "</div><!-- /.direct-chat-msg -->" +
-		    "</div><!--/.direct-chat-messages-->" +
 		  "</div><!-- /.box-body -->"+
           "<div class=\"box-footer\">"+
             "<div class=\"input-group\">"+
