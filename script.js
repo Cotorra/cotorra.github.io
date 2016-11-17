6df1181e23b1590d573b53d3d71180b24f73f5ef
asperduti <arielbmx@gmail.com>
2016-11-17 07:40:25 -0300

    Corrijo direccion de avatar

--- script.js
+++ script.js
@@ -62,10 +62,10 @@ function initSocketio(){
            $('#chat').append(
                   '<div class="direct-chat-msg">'+
                      '<div class="direct-chat-info clearfix">'+
-                        '<span class="direct-chat-name pull-left">'+ctagentName+'</span>'+
+                        '<span class="direct-chat-name pull-left">'+ ctagentName +'</span>'+
                         '<span class="direct-chat-timestamp pull-right">'+ datenow +'</span>'+
                      '</div>'+
-                     '<img class="direct-chat-img" src=\"'+ctAppURL+ctagentAvatar+'\" alt="message user image">'+
+                     '<img class="direct-chat-img" src=\"'+ ctagentAvatar +'\" alt="message user image">'+
                      '<div class="direct-chat-text">'+
                         data.msg +
                      '</div>'+
